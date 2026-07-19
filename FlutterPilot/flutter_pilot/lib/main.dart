efcbc4a5e73722d8df474611871c64ef161ca7a1
Baptiste TISSOT <baptistetissot@hotmail.com>
2026-07-19 21:18:02 +0200

    fix(wifi): handle lost connections

--- FlutterPilot/flutter_pilot/lib/main.dart
+++ FlutterPilot/flutter_pilot/lib/main.dart
@@ -24,7 +24,7 @@ class SimplePilot extends StatelessWidget {
   }
 }
 
-// ToDo
+// TODO
 // Show connection status as a top bar
 // Send and display Kp,Ki,Kd, IP Addr
 // Improve info display
@@ -32,5 +32,7 @@ class SimplePilot extends StatelessWidget {
 // * Rework UI to allow the definition of coefficients per speed
 // * Align with interface specification after spec rework
 // * Rework first screen UI to add waypoint mode (mode selector, current waypoint info, route selection  ...)
-// * Prevent auto screen lock
-// * WiFi vs 4G issue
+// * WiFi disco issue
+// * Set Kp Ki Kd
+// * Modify Manu cmd with speed
+// * Add settings for step for both auto/manu
